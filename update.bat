5919bc4b3ee5b0a92881e5496aa469b22f44e042
Matthew Clark <43628254+mon5termatt@users.noreply.github.com>
2024-03-12 01:56:32 -0500

    Update update.bat

--- update.bat
+++ update.bat
@@ -8,7 +8,7 @@ cls
 echo.Updating. Please wait while the new file is downloaded.
 powershell -c "$data = curl https://api.github.com/repos/mon5termatt/bg3-mods/git/refs/tag -UseBasicParsing | ConvertFrom-Json; $data[-1].ref -replace 'refs/tags/', '' | Out-File -Encoding 'UTF8' -FilePath './ver.ini'"
 set /p ver= < ver.ini
-set ver=%ver:~-8%
+set ver=%ver:~-6%
 del ver.ini /Q
 echo.Version %ver% found
 curl https://github.com/mon5termatt/bg3-mods/releases/download/%ver%/installmods.cmd -o installmods.cmd -q -L
