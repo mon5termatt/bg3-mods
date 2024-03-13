0356b8c72134d37cfe0ed1143a4ad94b49deb1a5
Matthew Clark <43628254+mon5termatt@users.noreply.github.com>
2024-03-12 20:31:24 -0500

    Update installmods.cmd

--- installmods.cmd
+++ installmods.cmd
@@ -1,7 +1,7 @@
 @echo off
 setlocal enabledelayedexpansion
 mode con:cols=100 lines=18
-set localver=031224
+set localver=1
 set "targetFolder=SteamLibrary\steamapps\common\Baldurs Gate 3"
 set "GOGFolder=GOG GAMES\Baldurs Gate 3"
 
