d2684f300baf1d88265ed9d5b92184d2a72fe413
Matthew Clark <43628254+mon5termatt@users.noreply.github.com>
2024-03-23 04:32:39 -0500

    Add files via upload

--- installmods.cmd
+++ installmods.cmd
@@ -1,11 +1,18 @@
 @echo off
 setlocal enabledelayedexpansion
-mode con:cols=100 lines=22
-set localver=0002
+mode con:cols=100 lines=40 
+
+::these change whenever the game or script updates
+set localver=0003
 ::built for game version
 set gamever=4.1.1.4905117
+
+::in case anything changes leave this as a variable.
 set "targetFolder=SteamLibrary\steamapps\common\Baldurs Gate 3"
 set "GOGFolder=GOG GAMES\Baldurs Gate 3"
+
+::this is not used yet
+if "%1" == "s" (set silent=true)
 
 
 :checkupdate
@@ -16,6 +23,7 @@ set /p remver= < curver.ini
 set remver=%remver:~-4%
 del curver.ini /Q
 cls
+call :logo
 if "%localver%" EQU "%remver%" (
 echo.[42mScript is up to date[0m
 echo.
@@ -24,7 +32,6 @@ goto startup
 if "%localver%" GEQ "%remver%" (
 echo.[43mScript is running a version newer then on the github[0m
 echo.
-timeout 3 > nul
 goto startup
 )
 
@@ -37,16 +44,27 @@ exit
 
 
 :startup
+::find the f**king game
+
+::if it didnt find it last launch, just go to manual
+if exist "%temp%/bg3mi.tmp" (
+del "%temp%\bg3mi.tmp"
+goto manual)
+
+
+::for the normies that dont have multiple drives
 if exist "%programfiles(x86)%\Steam\steamapps\common\Baldurs Gate 3\" (
-	echo.[42mFound at "%programfiles(x86)%\Steam\steamapps\common\Baldurs Gate 3[0m"
+	echo.[42mFound at "%programfiles(x86)%\Steam\steamapps\common\Baldurs Gate 3"[0m
 	set "gamepath=C:\Program Files (x86)\Steam\steamapps\common\Baldurs Gate 3"
 	goto :end_loop
 )
 if exist "%programfiles%\Steam\steamapps\common\Baldurs Gate 3\" (
-	echo.[42mFound at "%programfiles%\Steam\steamapps\common\Baldurs Gate 3[0m"
+	echo.[42mFound at "%programfiles%\Steam\steamapps\common\Baldurs Gate 3"[0m
 	set "gamepath=C:\Program Files\Steam\steamapps\common\Baldurs Gate 3"
 	goto :end_loop
 )
+
+::check all other drives in the normal folder steam makes for you.
 for %%I in (A B C D E F G H I J K L M N O P Q R S T U V W X Y Z) do (
     set "drive=%%I:"
     if exist "!drive!\%targetFolder%\" (
@@ -68,6 +86,7 @@ for %%I in (A B C D E F G H I J K L M N O P Q R S T U V W X Y Z) do (
 
 
 :manual
+::you are a f**king tech guy, you didnt just hit install in a normal place steam prompts you for.
 if not defined gamepath (
     echo Game directory not found automatically.
     echo Please enter your Game Directory: (e.g., E:\SteamLibrary\steamapps\common\Baldurs Gate 3)
@@ -75,9 +94,29 @@ if not defined gamepath (
 )
 
 :end_loop 
+::get to the f**king game directory, no matter what.
 cd /D %gamepath%
 
 :checkbackups
+
+
+::THIS CODE IS F**KING BROKEN
+
+::if this doesnt exist it means either the game is corrupt of it didnt find the right path
+::if not exist "%gamepath%\bin\bg3.exe" (
+
+::echo.Could not find the game files. Please make sure it detected the right folder.
+::echo.EXITING THE SCRIPT. FIX YOUR DRIVE. (make sure that the folder it detected above is right)
+::echo.If it found the wrong folder it means that you may have an old install there.
+::echo.verify you only have the game installed in ONE location.
+::echo.
+::echo.Relaunch the script and it will ask for the proper location
+::echo.>%temp%/bg3mi.tmp
+
+::write a file to let the script know that it couldnt find the proper install
+::on relaunch it will check for the file and just ask for the proper install location
+
+)
 
 if exist ".\bin\bg3.exe" (
 powershell "(Get-Item -path .\bin\bg3.exe).VersionInfo.ProductVersion">tmp
@@ -99,19 +138,26 @@ echo.
 echo.[44mBackup versions  = %bg3bak1% - %bg3bak2%[0m
 echo.[44mCurrent versions = %bg3cur1% - %bg3cur2%[0m
 echo.
-if %bg3cur1% NEQ %bg3bak1% (
-set mismatch=true
-echo.[41mBackup versions mismatch. was there a recent update?[0m
-) else (
-set mismatch=false
-echo.[42mBackup versions match.[0m
-)
+
 if %bg3cur1% NEQ %gamever% (
 echo.[41mThis script was built for game version[44m %gamever%[41m
 echo.Your current game version is not compatable with this build.
 echo.Please confirm that you have updated via Steam/GOG
 echo.if this persists you may need to verify your game files.[0m
 )
+if %bg3cur1% EQU %bg3bak1% (
+set mismatch=false
+echo.[42mBackup versions match.[0m
+) else (
+if exist ".\bin\bg3.exe.backup" (
+echo.[41mBackup versions mismatch. was there a recent update?
+echo.Plase run the install again using the update option.[0m
+) else (
+echo.[41mBackups Not found!
+echo.This could be because you dont have this modlist installed.[0m
+))
+if %bg3cur1% NEQ %bg3bak1% (
+set mismatch=true)
 
 
 ::pass in a variable from the console to automatically select 
@@ -146,14 +192,14 @@ powershell -command "Expand-Archive -Force '%gamepath%\files.zip' '%gamepath%'"
 del files.zip
 
 
-move "%gamepath%\appdata\ShowApprovalRatings - English.pak" "%localappdata%\Larian Studios\Baldur's Gate 3\Mods"
-move "%gamepath%\appdata\NoRomanceLimit.pak" "%localappdata%\Larian Studios\Baldur's Gate 3\Mods"
-move "%gamepath%\appdata\modsettings.lsx" "%localappdata%\Larian Studios\Baldur's Gate 3\PlayerProfiles\Public"
+move ".\appdata\ShowApprovalRatings - English.pak" "%localappdata%\Larian Studios\Baldur's Gate 3\Mods"
+move ".\appdata\NoRomanceLimit.pak" "%localappdata%\Larian Studios\Baldur's Gate 3\Mods"
+move ".\appdata\modsettings.lsx" "%localappdata%\Larian Studios\Baldur's Gate 3\PlayerProfiles\Public"
 
 
 :MPPREP
 
-if exist ".\bin\bg3.exe.backup" (
+if exist "%gamepath%\bin\bg3.exe.backup" (
 if %mismatch% EQU true (goto mppatch)
 if %mismatch% EQU false (goto skip)
 ) else (goto mppatch)
@@ -260,6 +306,31 @@ goto install
 goto exit)
 
 :exit
-echo.Press any key to exit.
-pause>nul
-exit
+
+echo.[92m       __               
+echo.  ____/ /___  ____  ___ 
+echo. / __  / __ \/ __ \/ _ \
+echo./ /_/ / /_/ / / / /  __/
+echo.\__,_/\____/_/ /_/\___/ [0m
+pause
+exit /b
+
+
+
+
+:logo
+echo.[1m
+echo.[92m    ____  ___________   [33m __  ___          __ [35m  ____           __        ____         
+echo.[92m   / __ )/ ____/__  /  [33m /  \/  /___  ____/ /[35m  /  _/___  _____/ /_____ _/ / /__  _____
+echo.[92m  / __  / / __  /_ \  [33m / /\_/ / __ \/ __  /[35m   / // __ \/ ___/ __/ __ `/ / / _ \/ ___/
+echo.[92m / /_/ / /_/ /___/ / [33m / /  / / /_/ / /_/ /[35m  _/ // / / (__  ) /_/ /_/ / / /  __/ /    
+echo.[92m/_____/\____//____/ [33m /_/  /_/\____/\__,_/[35m  /___/_/ /_/____/\__/\__,_/_/_/\___/_/     [0m
+echo.[1m
+echo.[36m    __          [31m   ______                        ________                            __  __ 
+echo.[36m   / /_  __  __[31m   / ____ \____ ___  ____  ____  / ____/ /____  _________ ___  ____ _/ /_/ /_
+echo.[36m  / __ \/ / / /[31m  / / __ `/ __ `__ \/ __ \/ __ \/___ \/ __/ _ \/ ___/ __ `__ \/ __ `/ __/ __/
+echo.[36m / /_/ / /_/ /[31m  / / /_/ / / / / / / /_/ / / / /___/ / /_/  __/ /  / / / / / / /_/ / /_/ /_  
+echo.[36m/_.___/\__, / [31m  \ \__,_/_/ /_/ /_/\____/_/ /_/_____/\__/\___/_/  /_/ /_/ /_/\__,_/\__/\__/  
+echo.[36m      /____/  [31m   \____/                                                                     [0m
+echo.
+exit /b
